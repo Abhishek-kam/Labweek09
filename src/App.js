28c26957e21cfd7aa0708f8d3b7c4cd3abf1c609
Abhishek-kam <mrfantastff@gmail.com>
2024-11-03 11:16:26 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <p>
+          React JS Programming Week09 Lab exercise
+        </p>
+        <p>101422675</p>
+        <p>Abhishek Kamat</p>
+        <p>George Brown College, Toronto</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
